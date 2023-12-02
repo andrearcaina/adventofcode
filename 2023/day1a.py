147930d54965ed6504e57c4d452dae81f9b22c1e
Andre <dtandre331@gmail.com>
2023-12-01 22:10:00 -0500

    readability

--- 2023/day1a.py
+++ 2023/day1a.py
@@ -4,7 +4,6 @@ with open('day1.txt', 'r') as f:
     for line in f:
         digits = [char for char in line if char.isdigit()]
 
-        digit = digits[0]+digits[-1]
-        sum += int(digit)
+        sum += int(digits[0]+digits[-1])
 
 print(sum)
