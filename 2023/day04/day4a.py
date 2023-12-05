b3664c5be9a193c11eff771d0ef0fc05c6a11472
Andre <dtandre331@gmail.com>
2023-12-04 21:19:29 -0500

    optimized algo
    W✅

--- 2023/day04/day4a.py
+++ 2023/day04/day4a.py
@@ -4,16 +4,10 @@ with open('day4.txt', 'r') as f:
     for line in f:
         line = line.strip().split(': ')[1].split("|")
         
-        current_numbs = []
-        winning_numbs = []
+        current_numbs = {int(numb) for numb in line[0].split()}
+        winning_numbs = {int(numb) for numb in line[1].split()}
 
-        for index, elem in enumerate(line):
-            elem = elem.replace(' ', ',').strip(',')
-            elems = [int(x) for x in elem.split(',') if x]
-            if index == 0: current_numbs = elems
-            else: winning_numbs = elems
-
-        correct_numbs = [numb for numb in current_numbs if numb in winning_numbs]
+        correct_numbs = current_numbs.intersection(winning_numbs)
 
         if correct_numbs:
             points += 2**(len(correct_numbs)-1)
